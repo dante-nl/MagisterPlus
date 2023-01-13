d2b7a4e263a242ae53835436022b39c410f4adce
dante-nl <61943433+dante-nl@users.noreply.github.com>
2023-01-13 11:08:56 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,89 @@
+//* Toggle switches
+// Onthoud keuzes en stel instellingen in
+
+// Highlighting bad grades
+chrome.storage.local.get("negativeHighlight", function(items) {
+	if (items["negativeHighlight"] == true) {
+		document.getElementById("negativeGradeToggle").checked = true
+	} 
+})
+
+document.getElementById("negativeGradeToggle").oninput = function () { toggleGradeHighlighting() };
+function toggleGradeHighlighting() {
+	chrome.storage.local.get("negativeHighlight", function(items) {
+		if (items["negativeHighlight"] == true) {
+			chrome.storage.local.remove(["negativeHighlight"])
+		} else {
+			save_content = {
+				"negativeHighlight": true
+			}
+			chrome.storage.local.set(save_content)
+		}
+	})
+}
+
+function highlightStatus() {
+	chrome.storage.local.get("negativeHighlight", function(items) {
+		console.log(items["negativeHighlight"])
+	});
+}
+
+// Showing average grade
+chrome.storage.local.get("showAverageGrade", function(items) {
+	if (items["showAverageGrade"] == true) {
+		document.getElementById("totalAverageToggle").checked = true
+	} 
+})
+
+document.getElementById("totalAverageToggle").oninput = function () { toggleAverage() };
+function toggleAverage() {
+	chrome.storage.local.get("showAverageGrade", function(items) {
+		if (items["showAverageGrade"] == true) {
+			chrome.storage.local.remove(["showAverageGrade"])
+		} else {
+			save_content = {
+				"showAverageGrade": true
+			}
+			chrome.storage.local.set(save_content)
+		}
+	})
+}
+
+function averageStatus() {
+	chrome.storage.local.get("showAverageGrade", function(items) {
+		console.log(items["showAverageGrade"])
+	});
+}
+
+// Custom theme
+chrome.storage.local.get("customTheme", function(items) {
+	if (items["customTheme"] == true) {
+		document.getElementById("themeToggle").checked = true
+	} 
+})
+
+document.getElementById("themeToggle").oninput = function () { toggleTheme() };
+function toggleTheme() {
+	chrome.storage.local.get("customTheme", function(items) {
+		if (items["customTheme"] == true) {
+			chrome.storage.local.remove(["customTheme"])
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.tabs.reload(tabs[0].id);
+			});
+		} else {
+			save_content = {
+				"customTheme": true
+			}
+			chrome.storage.local.set(save_content)
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.tabs.reload(tabs[0].id);
+			});
+		}
+	})
+}
+
+function themeStatus() {
+	chrome.storage.local.get("customTheme", function(items) {
+		console.log(items["customTheme"])
+	});
+}
